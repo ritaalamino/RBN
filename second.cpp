1f4c04988e72bcd329d3ef181198628186a2c3bb
ritaalamino <ritaalamino@gmail.com>
2019-05-20 11:17:58 -0300

    v_2005

--- second.cpp
+++ second.cpp
@@ -5,26 +5,29 @@ using namespace std;
 #include <bits/stdc++.h>
 //#include <QVector>
 #include <ctime>
+#define steps 10
 
 map<int, function<int(int, int)>> logic_map{
-        {0, [](int a, int b) { return a && b; }},
-        {1, [](int a, int b) { return a || b; }},
-        {2, [](int a, int b) { return !a; }}
+/*AND*/ {0, [](int a, int b) { return a && b; }},
+/*OR*/  {1, [](int a, int b) { return a || b; }},
+/*NAND*/{2, [](int a, int b) { return !(a && b);}},
+/*NOR*/ {3, [](int a, int b) { return !(a || b);}}
     };
+
 
 class Node{
     private:
         int state = 0;
     public:
-        Node *left;
-        Node *right;
+        //Node *left;
+        //Node *right;
 
         Node(){
-           left = right = NULL;
+           //left = right = NULL;
            this->state = rand()%2; //inicializa o nó com um valor aleatório
         };
         Node(int v){
-            left = right = NULL;
+            //left = right = NULL;
             this->state = v;
         }
         int r_state(){
@@ -44,15 +47,16 @@ class Graph{
         vector<vector<int>> f_Logica; //matriz de funções lógicas que pode ser substituida por vetor.
         Node *vertex_node; //Nó que guarda estado
         vector<vector<Node>> h_States; //Matriz historico de estados
-        Graph(int v_n){ //entra com o numero de nós
-            srand(1);
+        
+        Graph(int v_n,int r){ //entra com o numero de nós
+            srand(r);
             this->vertex_num = v_n;
             conex_Graph = new int *[vertex_num];
             vertex_node = new Node[vertex_num];
             for (int i = 0; i < vertex_num; i++){
                 h_States.push_back(vector<Node>()); //suposta inicialização do historico de estados
                 vertex_node[i] = Node();
-                h_States[0].push_back(vertex_node[i].r_state());
+                h_States[0].push_back(vertex_node[i]);
                 //cout << h_States[0][i].r_state() << "teste" << endl;
                 f_Logica.push_back(vector<int>());
                 //f_Logica[i] = new int[vertex_num];
@@ -61,9 +65,10 @@ class Graph{
                     //f_Logica[i][j] = rand()%3;
                     conex_Graph[i][j] = rand()%2;
                     if (conex_Graph[i][j]==1){
-                        f_Logica[i].push_back(rand()%3);
+                        f_Logica[i].push_back(rand()%4);
                     }
                 }
+                
             }
         }
 
@@ -77,7 +82,6 @@ class Graph{
                     if (this->conex_Graph[i][j] == 1){
                         aux.push_back(v_node(j));
                         //cout<< aux[j];
-                        
                     }
                     //cout << "\n";
                 }
@@ -90,20 +94,28 @@ class Graph{
                     aux.erase(aux.begin());
                 }
                 this->vertex_node[i].w_state(aux[0]);
-                h_States[1].push_back(vertex_node[i].r_state());
+                h_States[i+1].push_back(vertex_node[i]);
+                //cout << h_States[1][i].r_state() << "teste" << endl;
                 aux.clear();
             }
         }
 
-        int h_distance(){
-            int sum = 0;
-            for(int i = 0;i < this->h_States[i].size() ;i++){
-               if (this->h_States[0][i].r_state() != this->h_States[1][i].r_state()){
-                   sum+=1;
-               }
+        double h_distance(Graph b){
+            double sum = 0;
+            float h_dist;
+            for(int i = 0;i < this->vertex_num ;i++){
+                for(int j = 0; j<this->vertex_num; j++){
+                    if (this->h_States[i][j].r_state() != b.h_States[i][j].r_state()){
+                        sum+=1;                   
+                    }
+                }
             }
-            this->h_States[0] = this->h_States[1];
-            return sum;
+            //cout << sum << "sum ";
+            h_dist = (sum/steps);
+            //cout << " -- -- " << h_dist;
+            //this->h_States[0] = this->h_States[1];
+            //this->h_States[1].clear();
+            return h_dist;
         }
 
         int v_size(){
@@ -126,14 +138,14 @@ class Graph{
         cout << "\n";
         }
         void states(){
-        cout<<"Estados: ";
-        for (int i = 0; i < vertex_num; i++)
-        {
-                cout << v_node(i) << " ";
+            cout<<"Estados: ";
+            for (int i = 0; i < this->vertex_num; i++)
+            {
+                    cout << v_node(i) << " ";
+            }
+            cout << "\n";
+        }
 
-        }
-        cout << "\n";
-        }
 
         void logica(){
             for(vector<int> row: this->f_Logica){
@@ -147,11 +159,17 @@ class Graph{
 };
 
 int main(){
-    Graph m(20);
+    Graph m(10,1);
+    Graph n(10,3);
     m.states();
-    for(int i =0; i < 100;i++){
+    n.states();
+    for(int i =0; i < steps; i++){
         m.f_rand();
+        n.f_rand();
+        cout << "m ";
         m.states();
-        cout << "Ham. Dist. " << m.h_distance() <<endl;
+        cout << "n ";
+        n.states();
     }      
+    cout << "Ham. Dist. " << m.h_distance(n) <<endl;
 }
