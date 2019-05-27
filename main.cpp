72f630caff07b62f5a75fd5e39cea38db4fc7e77
ritaalamino <ritaalamino@gmail.com>
2019-05-27 09:38:58 -0300

    Version 27/05 Probability Matrix

--- main.cpp
+++ main.cpp
@@ -54,6 +54,7 @@ class Graph{
     private:
         int vertex_num;
         int **conex_Graph; //Grafo de conexão
+        float **prob_Graph; //Grafo de probabilidade da conexão
         Node *vertex_node; //Nó que guarda estado
         vector<vector<int>> f_Logica; //matriz de funções lógicas que pode ser substituida por vetor
         vector<vector<Node>> h_States; //Matriz historico de estados
@@ -64,14 +65,17 @@ class Graph{
             srand(r);
             this->vertex_num = v_n;
             this->conex_Graph = new int *[vertex_num];
+            this->prob_Graph = new float *[vertex_num];
             this->vertex_node = new Node[vertex_num];
             for (int i = 0; i < vertex_num; i++){
+                this->prob_Graph[i] = new float[vertex_num];
                 this->h_States.push_back(vector<Node>()); //suposta inicialização do historico de estados
                 this->vertex_node[i] = Node();
                 this->h_States[0].push_back(vertex_node[i]); //it registers first line at hisotirc
                 this->f_Logica.push_back(vector<int>());
                 this->conex_Graph[i] = new int[vertex_num];
                 for (int j = 0; j<vertex_num; j++){
+                    this->prob_Graph[i][j] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
                     this->conex_Graph[i][j] = rand()%2;
                     if (conex_Graph[i][j]==1){
                         this->f_Logica[i].push_back(rand()%4);
@@ -124,16 +128,28 @@ class Graph{
         int v_node(int i){
             return this->vertex_node[i].r_state();
         }
+        /*Print Probability*/
+        void prob(){
+            for (int i = 0; i < vertex_num; i++)
+            {
+                cout << "\n";
+                for (int j = 0; j < vertex_num; j++)
+                {
+                    cout << this->prob_Graph[i][j] << " ";
+                }
+            }
+        cout << "\n";
+        }
         /*Print Connections*/
         void conexions(){
-        for (int i = 0; i < vertex_num; i++)
-        {
-            cout << "\n";
-            for (int j = 0; j < vertex_num; j++)
+            for (int i = 0; i < vertex_num; i++)
             {
-                cout << this->conex_Graph[i][j] << " ";
+                cout << "\n";
+                for (int j = 0; j < vertex_num; j++)
+                {
+                    cout << this->conex_Graph[i][j] << " ";
+                }
             }
-        }
         cout << "\n";
         }
         /*Print States*/
@@ -161,11 +177,14 @@ class Graph{
 int main(){
     int v_n,v_s,v_s2;
     cout << "***********\nNumber of Vertex: ";   
-    cin >> v_n;
+    //cin >> v_n;
+    v_n = 10;
     cout << "***********\nRandom Seed 1: ";   
-    cin >> v_s;
+    //cin >> v_s;
+    v_s = 1;
     cout << "***********\nRandom Seed 2: ";   
-    cin >> v_s2;
+    v_s2 = 2;
+    //cin >> v_s2;
     Graph m(v_n,v_s);
     Graph n(v_n,v_s2);
     cout << "m ";
